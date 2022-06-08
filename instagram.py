e28dcb0868db8e1abd254fb454234cc8f6b4da3d
frediyan erick ernando <35546027+fxhfdhgdj@users.noreply.github.com>
2022-06-08 21:27:36 +0700

    Delete instagram.py

--- instagram.py
+++
@@ -1,7 +0,0 @@
-import os
-os.system('git pull')
-if __name__ == "__main__":
-        try:
-                __import__("igku").checkin()
-        except Exception as e:
-                exit(str(e))
